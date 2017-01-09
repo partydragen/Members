e419ed4fdadbd3869d64ce35fbc15875d25fec66
partydragen <adrian-kili@hotmail.no>
2017-01-09 22:09:36 +0100

    Add files via upload

---
+++ Members/language/EnglishUK/members.php
@@ -0,0 +1,14 @@
+<?php 
+/*
+ *	Made by Partydragen
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-pr2
+ *
+ *  License: MIT
+ *
+ *  EnglishUK Language for Members module
+ */
+
+$language = array( 
+	'members' => 'Members'
+);
---
+++ Members/module.php
@@ -0,0 +1,13 @@
+<?php 
+/*
+ *	Made by Partydragen
+ *  https://github.com/NamelessMC/Nameless/
+ *  NamelessMC version 2.0.0-dev
+ *
+ *  License: MIT
+ *
+ *  Members List module info file
+ */
+
+$module_version = '2.0.0';
+$nameless_version = '2.0.0-pr2';
