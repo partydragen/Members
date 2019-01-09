6b929d0cec6b3fe6915bd1ff8b47ce1b28cdbba0
partydragen <admin@partydragen.com>
2019-01-09 20:16:47 +0100

    Polish translation, credit brzezinsky

---
+++ upload/modules/Members/language/Polish/members.php
@@ -0,0 +1,23 @@
+<?php 
+/*
+ *	Made by Partydragen And Samerton
+ *  https://github.com/partydragen/Members/
+ *  https://partydragen.com/
+ *  NamelessMC version 2.0.0-pr5
+ *  License: MIT
+ *
+ *  Translated by brzezinsky
+ *
+ *  Polish Language - Members module
+ *  Język polski - Moduł listy użytkowników
+ */
+$language = array( 
+	'members' => 'Użytkownicy',
+	'username' => 'Nazwa użytkownika',
+	'group' => 'Grupa',
+	'created' => 'Rejestracja',
+	'link_location' => 'Lokalizacja linku',
+	'icon' => 'Ikona',
+	'icon_example' => 'Przykład: <i class="fa fa-users" aria-hidden="true"></i>',
+	'all' => 'Wszyscy'
+);
