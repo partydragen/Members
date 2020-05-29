97ac5d7625d20a74df96e1647226925506168ffb
zJerino <48019027+zJerino@users.noreply.github.com>
2020-05-29 16:40:18 -0400

    Create members.php

---
+++ upload/modules/Members/language/Spanish/members.php
@@ -0,0 +1,22 @@
+<?php 
+/*
+ *	Made by Partydragen And Samerton
+ *  https://github.com/partydragen/Members/
+ *  https://partydragen.com/
+ *  NamelessMC version 2.0.0-pr5
+ *
+ *  License: MIT
+ *
+ *  EnglishUK Language for Members module
+ */
+
+$language = array( 
+	'members' => 'Miembros',
+	'username' => 'Usuario',
+	'group' => 'Grupo',
+	'created' => 'Registrado',
+	'link_location' => 'Ubicación del enlace',
+	'icon' => 'Icono',
+	'icon_example' => 'ejemplo: <i class="fa fa-users" aria-hidden="true"></i>',
+	'all' => 'Todo'
+);
